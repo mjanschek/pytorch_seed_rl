a11b2b3bdd83e2b55664944a2d84b34f80ac9fd8
Michael Janschek <michael.janschek@gmx.de>
2020-06-09 14:22:22 +0200

    refine travis setup

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 import setuptools
 
+INSTALL_REQUIRES = [
+    'torch>=1.5',
+    'gym[atari]>=0.17.2',
+    'torchsummary>=1.5.1'
+]
+
+TEST_REQUIRES = [
+    # testing and coverage
+    'pytest', 'coverage', 'pytest-cov',
+    # unmandatory dependencies of the package itself
+    
+    # to be able to run `python setup.py checkdocs`
+    'collective.checkdocs', 'pygments',
+]
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="pytorch-seed-rl", 
+    license="MIT",
     version="0.0.1",
     author="Michael Janschek",
     author_email="michael.janschek@hhu.de",
@@ -13,10 +29,15 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/mjanschek/pytorch_seed_rl",
     packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=INSTALL_REQUIRES,
+    extras_require={
+        'test': TEST_REQUIRES + INSTALL_REQUIRES,
+    },
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    python_requires='>=3.6',
 )
