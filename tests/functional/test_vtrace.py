b521e8d4f3aaaf6c4088e288e9b39d920934c46e
Michael Janschek <michael.janschek@gmx.de>
2020-11-04 13:11:29 +0100

    add placeholder tests

---
+++ tests/functional/test_vtrace.py
@@ -0,0 +1,13 @@
+from pytorch_seed_rl.functional import vtrace
+
+import numpy as np
+import torch
+
+def test_action_log_probs():
+    assert True
+
+def test_from_logits():
+    assert True
+
+def test_from_importance_weights():
+    assert True
