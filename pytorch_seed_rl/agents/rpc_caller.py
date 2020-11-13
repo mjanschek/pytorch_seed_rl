b4d82b597039410714f6f2a85d4737d3c83017b1
Michael Janschek <michael.janschek@gmx.de>
2020-11-13 10:28:39 +0100

    write commentary for RpcCaller

--- pytorch_seed_rl/agents/rpc_caller.py
+++ pytorch_seed_rl/agents/rpc_caller.py
@@ -15,8 +15,11 @@
 """RPC object that handles communication with an assigned RPC callee.
 """
 from torch.distributed import rpc
+from torch.futures import Future
 
 from abc import abstractmethod
+
+from .. import agents
 
 
 class RpcCaller():
@@ -24,6 +27,11 @@ class RpcCaller():
     """
 
     def __init__(self, rank, callee_rref):
+        # ASSERTIONS
+        # check for RpcCallee being inherited by callee_rref
+        assert issubclass(callee_rref._get_type(),
+                          agents.rpc_callee.RpcCallee)
+
         self.callee_rref = callee_rref
 
         self.id = rpc.get_worker_info().id
@@ -33,21 +41,44 @@ class RpcCaller():
         self.shutdown = False
 
     def loop(self):
-        """Loop acting method.
+        """Main loop function of an RpcCaller.
+
+        # . Checks in with assigned '~RpcCallee'.
+        # . Loops '~_loop()' until '~self.shutdown' is set True.
+        # . Checks out with assigned '~RpcCallee'.
         """
         self.callee_rref.rpc_sync().check_in(self.rank)
 
-        self._loop()
+        while not self.shutdown:
+            self._loop()
 
         self.callee_rref.rpc_sync().check_out(self.rank)
-
-        return True
+        self.cleanup()
 
     @abstractmethod
     def _loop(self):
+        """Inner loop function of an RpcCaller. Called by '~loop()'
+
+        Must be implemented by child class
+        """
         raise NotImplementedError
 
-    def batched_rpc(self, *args, **kwargs):
+    def batched_rpc(self, *args, **kwargs) -> Future:
         """Wrap for batched async RPC ran on remote callee.
         """
         return self.callee_rref.rpc_async().batched_process(*args, **kwargs)
+
+    def cleanup(self):
+        """Cleans up after main loop is done. Called by '~cleanup()'
+
+        Calls abstract method '~_cleanup()'
+        """
+        self._cleanup()
+
+    @abstractmethod
+    def _cleanup(self):
+        """Cleans up after main loop is done. Called by '~cleanup()'
+
+        Must be implemented by child class.
+        """
+        raise NotImplementedError
