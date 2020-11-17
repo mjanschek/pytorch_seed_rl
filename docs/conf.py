54a5387044eac73b398914fd42f2a69af2738209
Michael Janschek <michael.janschek@gmx.de>
2020-11-17 12:20:34 +0100

    update docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -96,14 +96,14 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_rtd_theme'
 
-html_theme_options = {
-    'collapse_navigation': False,
-    'sticky_navigation': False,
-}
+# html_theme_options = {
+#     'collapse_navigation': False,
+#     'sticky_navigation': False,
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_show_sourcelink = True
+# html_show_sourcelink = True
