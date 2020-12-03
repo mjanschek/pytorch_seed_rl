bad4915287f75ac27a10d24a70d160d6628c2899
Michael Janschek <michael.janschek@gmx.de>
2020-12-03 17:02:36 +0100

    finish docs

--- pytorch_seed_rl/tools/trajectory_store.py
+++ pytorch_seed_rl/tools/trajectory_store.py
@@ -19,9 +19,10 @@ import copy
 from typing import List, Union
 
 import torch
-from torch.multiprocessing import Lock
+from torch.multiprocessing import Lock, Queue
 
 from .functions import dict_to_device, listdict_to_dictlist
+from .recorder import Recorder
 
 
 class TrajectoryStore():
@@ -44,29 +45,24 @@ class TrajectoryStore():
         A dictionary with the exact shape of data that shall be stored.
     device: `torch.device`
         The :py:obj:`torch.device` this stores data is stored on.
+    recorder: :py:class:`~.Recorder`
+        A :py:class:`~.Recorder` object that logs and records data.
     trajectory_length: `int`
         The number of states a trajectory shall contain when completed.
-    max_queued_drops: `int`
-        The maximum number of dropped trajectories waiting in the queue.
-
-    Attributes
-    ----------
-    drop_off_queue: `deque`
-        A queue for dropped (complete) trajectories
     """
 
     def __init__(self,
                  keys: List[Union[int, str]],
                  zero_obs: dict,
                  device: torch.device,
-                 out_queue,
-                 recorder,
+                 out_queue: Queue,
+                 recorder: Recorder,
                  trajectory_length: int = 128):
         # ATTRIBUTES
         self.out_queue = out_queue
         self.device = device
-        self.trajectory_length = trajectory_length
         self.recorder = recorder
+        self._trajectory_length = trajectory_length
 
         self.zero_obs = {k: v.to(self.device) for k, v in zero_obs.items()}
         self.zero_obs['episode_id'] = torch.ones(
@@ -76,29 +72,27 @@ class TrajectoryStore():
         self._reset_states(self.zero_obs)
 
         # Counters
-        self.trajectory_counter = 0
-        self.episode_counter = len(keys)
+        self._trajectory_counter = 0
+        self._episode_counter = len(keys)
 
         # Setup storage
-        self.internal_store = {k: self._new_trajectory() for k in keys}
-        self.episode_id_store = {k: i for i, k in enumerate(keys)}
-        self.locks_trajectories = {k: Lock() for k in keys}
-        self.lock_episode_counter = Lock()
-
-
+        self._internal_store = {k: self._new_trajectory() for k in keys}
+        self._episode_id_store = {k: i for i, k in enumerate(keys)}
+        self._locks_trajectories = {k: Lock() for k in keys}
+        self._lock_episode_counter = Lock()
 
     def _new_trajectory(self) -> dict:
         """Returns a new, empty trajectory.
         """
         trajectory = {
-            "trajectory_id": torch.tensor(self.trajectory_counter, device=self.device),
+            "trajectory_id": torch.tensor(self._trajectory_counter, device=self.device),
             "complete": torch.tensor(False, device=self.device),
             "current_length": torch.tensor(0, device=self.device),
             "states": self._new_states(),
             "metrics": list()
         }
 
-        self.trajectory_counter += 1
+        self._trajectory_counter += 1
 
         return trajectory
 
@@ -108,7 +102,7 @@ class TrajectoryStore():
         states = {}
         for key, value in self.zero_obs.items():
             states[key] = value.repeat(
-                (self.trajectory_length,) + (1,)*(len(value.size())-1))
+                (self._trajectory_length,) + (1,)*(len(value.size())-1))
         return states
 
     def _reset_trajectory(self, trajectory: dict):
@@ -119,9 +113,9 @@ class TrajectoryStore():
         trajectory: `dict`
             A trajectory as produced by :py:class:`pytorch_seed_rl.agents.Actor`
         """
-        self.trajectory_counter += 1
+        self._trajectory_counter += 1
 
-        trajectory['trajectory_id'].fill_(self.trajectory_counter)
+        trajectory['trajectory_id'].fill_(self._trajectory_counter)
         trajectory['current_length'].fill_(0)
         trajectory['metrics'] = list()
 
@@ -157,13 +151,13 @@ class TrajectoryStore():
         metrics: `dict`
             An optional dictionary containing additional values.
         """
-        self.locks_trajectories[key].acquire()
+        self._locks_trajectories[key].acquire()
 
         # all keys must be known to store
         assert all(k in self.zero_obs.keys() for k in state.keys())
 
         # load known trajectory
-        internal_trajectory = self.internal_store[key]
+        internal_trajectory = self._internal_store[key]
         length = internal_trajectory['current_length'].item()
         internal_states = internal_trajectory['states']
 
@@ -189,22 +183,23 @@ class TrajectoryStore():
             print("state[%s] under store key %s is not 0!" % (k, key))
 
         # save old episode_id
-        old_eps_id = self.episode_id_store[key]
+        old_eps_id = self._episode_id_store[key]
         if state['done']:
-            with self.lock_episode_counter:
-                self.episode_counter += 1
-                self.episode_id_store[key] = self.episode_counter
+            with self._lock_episode_counter:
+                self._episode_counter += 1
+                self._episode_id_store[key] = self._episode_counter
 
         # update info
-        internal_states['episode_id'][length].fill_(self.episode_id_store[key])
+        internal_states['episode_id'][length].fill_(
+            self._episode_id_store[key])
         internal_states['prev_episode_id'][length].fill_(old_eps_id)
 
         internal_trajectory['current_length'] += 1
-        if internal_trajectory['current_length'] == self.trajectory_length:
+        if internal_trajectory['current_length'] == self._trajectory_length:
             self._drop(copy.deepcopy(internal_trajectory))
             self._reset_trajectory(internal_trajectory)
 
-        self.locks_trajectories[key].release()
+        self._locks_trajectories[key].release()
 
     def _drop(self,
               trajectory: dict):
